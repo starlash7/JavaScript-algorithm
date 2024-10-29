e5bdbb79572be09005485062edb1ec88a3c9472e
starlash7 <yang92sw@gmail.com>
2024-10-29 15:22:25 +0900

    첫번째 알고리즘

---
+++ CH-2/2-2.js
@@ -0,0 +1,11 @@
+// A+B
+
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let line = input[0].split(" ");
+
+let a = parseInt(line[0]); //1
+let b = parseInt(line[1]); //2
+
+console.log(a + b);
